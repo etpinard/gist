4172a0c8682b53fcc471c3dca803a596ab504dd8
Étienne Tétreault-Pinard <etienne.t.pinard@gmail.com>
2020-07-20 18:32:04 -0400

    refactor <div .sources>
    - show them always (i.e. no .hide())
    - show index.html last if minified
    - don't show 'thumbnail.png', 'preview.gif', '.gitignore', '.block'
      and 'package-lock.json'

--- js/get_gist_content.js
+++ js/get_gist_content.js
@@ -49,7 +49,7 @@
           .append(marked(gist.data.files['README.md'].content));
 
         // gist sources
-        var $sources = $('.gist-sources').hide();
+        var $sources = $('.gist-sources');
 
         var helper = function (k, v) {
           $('<div>')
@@ -61,16 +61,44 @@
             .appendTo($sources);
         };
 
-        helper('index.html', gist.data.files['index.html']);
-        delete gist.data.files['README.md'];
-        delete gist.data.files['index.html'];
-        delete gist.data.files['thumbnail.png'];
-        $.each(gist.data.files, helper);
-      }
-    });
+        var fileLookup = {};
 
-    $('.toggle-source').click(function () {
-      $('.gist-sources').toggle();
+        // then index.html (if not minified)
+        var isIndexHtmlMinified = false;
+        try {
+          var pkg = gist.data.files['package.json']
+          if (pkg.content.indexOf('indexhtmlify') !== -1) {
+            isIndexHtmlMinified = true;
+          }
+        } catch(e) {}
+        if (!isIndexHtmlMinified) {
+          fileLookup['index.html'] = gist.data.files['index.html'];
+        }
+
+        var fileList = Object.keys(gist.data.files).sort();
+        var badList = ['README.md', 'thumbnail.png', 'preview.gif', '.gitignore', '.block',
+                       'package.json', 'package-lock.json'];
+        for (var i = 0; i < fileList.length; i++) {
+          var k = fileList[i]
+          if (fileLookup[k]) continue;
+          if (k[0] === ' ') continue;
+          if (badList.indexOf(k) !== -1) continue;
+          if (isIndexHtmlMinified && k === 'index.html') continue;
+          fileLookup[k] = gist.data.files[k];
+        }
+
+        // then comes the package.json
+        if ('package.json' in gist.data.files) {
+          fileLookup['package.json'] = gist.data.files['package.json']
+        }
+
+        // minified index.html comes last
+        if (isIndexHtmlMinified) {
+          fileLookup['index.html'] = gist.data.files['index.html'];
+        }
+
+        $.each(fileLookup, helper);
+      }
     });
 
   });
